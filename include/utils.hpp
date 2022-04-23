03b3a671262c9d022aa695f7fed35c7f1ed63225
Winston H <56998716+winstxnhdw@users.noreply.github.com>
2022-04-23 17:25:32 +0800

    remove constexpr for compatible with ros

--- include/utils.hpp
+++ include/utils.hpp
@@ -6,6 +6,6 @@ typedef struct {
 } Vector2;
 
 // Returns the dot products of two vectors
-constexpr double dot(const Vector2 &A, const Vector2 &B) {
+double dot(const Vector2 &A, const Vector2 &B) {
     return A.x*B.x + A.y*B.y;
 }
