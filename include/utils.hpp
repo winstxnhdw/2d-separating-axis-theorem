e5167d32aec813613c7032b8f4b3a7808af3f2fc
Winston H <56998716+winstxnhdw@users.noreply.github.com>
2022-04-23 17:19:48 +0800

    Update utils.hpp

--- include/utils.hpp
+++ include/utils.hpp
@@ -6,6 +6,6 @@ typedef struct {
 } Vector2;
 
 // Returns the dot products of two vectors
-double dot(const Vector2 &A, const Vector2 &B) {
+constexpr double dot(const Vector2 &A, const Vector2 &B) {
     return A.x*B.x + A.y*B.y;
 }
