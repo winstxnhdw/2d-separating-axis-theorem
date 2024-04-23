80a09bca94671583779b7d921e7ee790bb31847c
Winston H <56998716+winstxnhdw@users.noreply.github.com>
2024-04-23 23:56:45 +0800

    refactor: do not recompute angles

--- main.cpp
+++ main.cpp
@@ -2,22 +2,24 @@
 #include "separating_axis.hpp"
 
 std::vector<Vector2> calculate_bounds(const double x, const double y, const double yaw, const double centre_to_front, const double centre_to_rear, const double centre_to_side) {
-    
+    const double sin_yaw = sin(yaw);
+    const double cos_yaw = cos(yaw);
+
     Vector2 front_right_point;
-    front_right_point.x = x + centre_to_side * sin(yaw) + centre_to_front * cos(yaw);
-    front_right_point.y = y - centre_to_side * cos(yaw) + centre_to_front * sin(yaw);
+    front_right_point.x = x + centre_to_side * sin_yaw + centre_to_front * cos_yaw;
+    front_right_point.y = y - centre_to_side * cos_yaw + centre_to_front * sin_yaw;
 
     Vector2 front_left_point;
-    front_left_point.x = x - centre_to_side * sin(yaw) + centre_to_front * cos(yaw);
-    front_left_point.y = y + centre_to_side * cos(yaw) + centre_to_front * sin(yaw);
+    front_left_point.x = x - centre_to_side * sin_yaw + centre_to_front * cos_yaw;
+    front_left_point.y = y + centre_to_side * cos_yaw + centre_to_front * sin_yaw;
 
     Vector2 rear_left_point;
-    rear_left_point.x = x - centre_to_side * sin(yaw) - centre_to_rear * cos(yaw);
-    rear_left_point.y = y + centre_to_side * cos(yaw) - centre_to_rear * sin(yaw);
+    rear_left_point.x = x - centre_to_side * sin_yaw - centre_to_rear * cos_yaw;
+    rear_left_point.y = y + centre_to_side * cos_yaw - centre_to_rear * sin_yaw;
 
     Vector2 rear_right_point;
-    rear_right_point.x = x + centre_to_side * sin(yaw) - centre_to_rear * cos(yaw);
-    rear_right_point.y = y - centre_to_side * cos(yaw) - centre_to_rear * sin(yaw);
+    rear_right_point.x = x + centre_to_side * sin_yaw - centre_to_rear * cos_yaw;
+    rear_right_point.y = y - centre_to_side * cos_yaw - centre_to_rear * sin_yaw;
 
     return {front_right_point, front_left_point, rear_left_point, rear_right_point};
 }
